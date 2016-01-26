139dcb44f142442a6edc408b77b888c5bef06eff
Jay Whang and Michael Schennum <mikeschenphoto@gmail.com>
2016-01-26 11:58:23 -0800

    add user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,43 @@
-function Tickets(movieName, movieTime) {
+function Tickets(movieName, movieTime, movieAge) {
   this.movieName = movieName;
   this.movieTime = movieTime;
+  this.movieAge = movieAge;
+
 }
+
+Tickets.prototype.viewTicket = function () {
+  return this.movieName + " " + this.movieTime + " " + this.movieAge;
+}
+
+Tickets.prototype.totalTicket = function() {
+  var namePrice = 0;
+  if (this.movieName === "creed") {
+    namePrice = 10;
+  }
+  else {
+    namePrice = 12;
+  }
+  if (this.movieTime !== "8pm") {
+    namePrice = namePrice - 2;
+  }
+  if (this.movieAge !== "adult") {
+    namePrice = namePrice - 3;
+  }
+  return namePrice;
+};
+
+
+$(document).ready(function() {
+  $("form#movieName").submit(function(event) {
+    event.preventDefault();
+    $(".result").empty();
+    var movieName= $("select#movieChoice").val();
+    var movieTime = $("select#timeChoice").val();
+    var movieAge = $("select#ageChoice").val();
+    var newMovie = new Tickets(movieName, movieTime, movieAge);
+
+
+    $(".result").append(newMovie.totalTicket());
+
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,8 @@
 describe('Tickets', function() {
-  it("takes name of a movie and returns that name", function() {
-    var testMovie = new Tickets("Star Wars", "12 PM");
+  it("takes name of a movie and returns that name, time and age", function() {
+    var testMovie = new Tickets("Star Wars", "12 PM", 21);
     expect(testMovie.movieName).to.equal("Star Wars");
     expect(testMovie.movieTime).to.equal("12 PM");
+    expect(testMovie.movieAge).to.equal(21);
     });
-
-});
+  });
