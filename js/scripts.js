5936ffc2ea10f57ca270603cd9321a2ec1e3e812
Jay Whang and Michael Schennum <jaywhang83@gmail.com>
2016-01-26 13:15:38 -0800

    added hide show

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,16 @@ function Tickets(movieName, movieTime, movieAge) {
   this.movieName = movieName;
   this.movieTime = movieTime;
   this.movieAge = movieAge;
+};
 
-}
-
-Tickets.prototype.viewTicket = function () {
-  return this.movieName + " " + this.movieTime + " " + this.movieAge;
-}
+// Tickets.prototype.viewTicket = function () {
+//   return this.movieName + " " + this.movieTime + " " + this.movieAge;
+// }
 
 Tickets.prototype.totalTicket = function() {
-  var namePrice = 0;
+  var namePrice = 12;
   if (this.movieName === "creed") {
-    namePrice = 10;
-  }
-  else {
-    namePrice = 12;
+    namePrice = namePrice - 2;
   }
   if (this.movieTime !== "8pm") {
     namePrice = namePrice - 2;
@@ -26,7 +22,6 @@ Tickets.prototype.totalTicket = function() {
   return namePrice;
 };
 
-
 $(document).ready(function() {
   $("form#movieName").submit(function(event) {
     event.preventDefault();
@@ -36,8 +31,7 @@ $(document).ready(function() {
     var movieAge = $("select#ageChoice").val();
     var newMovie = new Tickets(movieName, movieTime, movieAge);
 
-
-    $(".result").append(newMovie.totalTicket());
-
+    $(".result").append(" $" + newMovie.totalTicket() + ".00");
+    $(".view").show();
   });
 });
