cb4c284940d1e60c50b3f8cc520c7442a8227291
Jay Whang and Michael Schennum <mikeschenphoto@gmail.com>
2016-01-26 13:28:07 -0800

    added method in an constructor

--- js/scripts.js
+++ js/scripts.js
@@ -2,25 +2,25 @@ function Tickets(movieName, movieTime, movieAge) {
   this.movieName = movieName;
   this.movieTime = movieTime;
   this.movieAge = movieAge;
+
+  this.totalTicket = function() {
+    var namePrice = 12;
+    if (this.movieName === "creed") {
+      namePrice = namePrice - 2;
+    }
+    if (this.movieTime !== "8pm") {
+      namePrice = namePrice - 2;
+    }
+    if (this.movieAge !== "adult") {
+      namePrice = namePrice - 3;
+    }
+    return namePrice;
+  };
 };
 
 // Tickets.prototype.viewTicket = function () {
 //   return this.movieName + " " + this.movieTime + " " + this.movieAge;
 // }
-
-Tickets.prototype.totalTicket = function() {
-  var namePrice = 12;
-  if (this.movieName === "creed") {
-    namePrice = namePrice - 2;
-  }
-  if (this.movieTime !== "8pm") {
-    namePrice = namePrice - 2;
-  }
-  if (this.movieAge !== "adult") {
-    namePrice = namePrice - 3;
-  }
-  return namePrice;
-};
 
 $(document).ready(function() {
   $("form#movieName").submit(function(event) {
